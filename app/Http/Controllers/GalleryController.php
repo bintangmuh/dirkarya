73f6eefe29579b5b147423b9fc9151f9eee54929
Bintang Muhammad <bintangmhd@gmail.com>
2017-04-16 18:32:13 +0700

    can upload, need fix

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -3,12 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
+use Redirect;
+use App\Galleries as Galleries;
+use Auth;
+use Carbon\Carbon as Carbon;
 
 class GalleryController extends Controller
 {
-    public function viewUploader() {
-      return view('karya.addimages');
+    public function viewUploader($id) {
+      $gallery = Galleries::where('karya_id', $id)->get();
+      return view('karya.addimages',
+      [
+        'id' => $id,
+        'galleries' => $gallery
+      ]);
     }
 
-    
+    public function upload($id, Request $request) {
+      $validator = Validator::make($request->all(), [
+        'imagegallery' => 'image|mimes:jpeg,jpg,png,gif'
+      ]);
+
+      if ($validator->fails()) {
+          return Redirect::back()
+                ->withErrors($validator)
+                ->withInput();
+      }
+
+      $filename = "gallery-karya-".$id."-".Carbon::now()->format('YmdHis').".".$request->file('imagegallery')->getClientOriginalExtension(); //save name of pictures
+      $path = $request->file('imagegallery')->storeAs('public', $filename); //store to public storage
+
+      $gallery = new Galleries();
+      $gallery->karya_id = $id;
+      $gallery->user_id = Auth::user()->id;
+      $gallery->img_url = "storage" . $filename; //write on database
+      $gallery->save();
+
+      return redirect()->route('karya', ['id' => $gallery->karya_id]);
+
+    }
+
+
 }
