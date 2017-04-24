552ec899f82d0a6edfb8bc4d7dd7d3ca10e93067
Bintang Muhammad <bintangmhd@gmail.com>
2017-04-24 21:26:19 +0700

    menambah gallery multiupload

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Validator;
 use Redirect;
 use App\Galleries as Galleries;
+use App\Karya as Karya;
 use Auth;
 use Carbon\Carbon as Carbon;
 
@@ -13,13 +14,44 @@ class GalleryController extends Controller
 {
     public function viewUploader($id) {
       $gallery = Galleries::where('karya_id', $id)->get();
+      $karya =  Karya::findOrFail($id);
       return view('karya.addimages',
       [
         'id' => $id,
+        'karya' => $karya,
         'galleries' => $gallery
       ]);
     }
 
+    public function multiupload($id, Request $request) {
+      $validator = Validator::make($request->all(), [
+        'imagegallery.*' => 'image|mimes:jpeg,jpg,png,gif'
+      ]);
+
+      if ($validator->fails()) {
+          return Redirect::back()
+                ->withErrors($validator)
+                ->withInput();
+      }
+
+      foreach ($request->file('imagegallery') as $input) {
+        $filename = "/gallery-karya-".$id."-".Carbon::now()->format('YmdHis').rand().".".$input->getClientOriginalExtension(); //save name of pictures
+        $path = $input->storeAs('public', $filename); //store to public storage
+
+        $gallery = new Galleries();
+        $gallery->karya_id = $id;
+
+        $gallery->user_id = Auth::user()->id;
+
+
+        $gallery->img_url = "storage" . $filename; //write on database
+        $gallery->save();
+
+      }
+
+      return Redirect::back()->with('success', 'Gambar telah berhasil ditambahkan');
+
+    }
     public function upload($id, Request $request) {
       $validator = Validator::make($request->all(), [
         'imagegallery' => 'image|mimes:jpeg,jpg,png,gif'
@@ -47,9 +79,10 @@ class GalleryController extends Controller
 
     public function removeimage($postid, $id) {
       $gallery = Galleries::findOrFail($id);
+      $name = $gallery->img_url;
       $gallery->delete();
 
-      return redirect()->back();
+      return redirect()->back()->with('success', 'Gambar ' .$name. ' telah berhasil dhapus');
     }
 
 }
--- resources/views/karya/view.blade.php
+++ resources/views/karya/view.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.app')
 
+@section('title',  $karya->nama)
 @section('content')
   <div class="container">
     <div class="row">
+      @if (Auth::check())
+        @if (Auth::user()->id === $karya->user->id)
+          <div class="col-sm-12" style="margin-bottom: 20px;">
+              <a href="{{ route('karyaeditview', ['id' => $karya->id]) }}" class="btn btn-primary"><i class="mdi mdi-pencil"></i> Edit Page</a>
+              <a href="{{ route('addimage', ['idlorem' => $karya->id]) }}" class="btn btn-primary"><i class="mdi mdi-image"></i> Tambah Gallery</a>
+          </div>
+        @endif
+      @endif
       <div class="col-sm-12">
         <div class="card karya-view">
           <div class="row">
@@ -17,9 +26,11 @@
           <div class="panel-action-karya col-sm-12">
             <a href="#" class="btn btn-primary">Kunjungi Karya</a>
           </div>
-          <ul class="gallery">
+          <ul class="col-sm-12 gallery">
             @foreach ($karya->images as $image)
-              <img src="{{ asset($image->img_url) }}" alt="" class="img-responsive" style="height: 300px; display: inline;">
+              <li>
+                <img src="{{ asset($image->img_url) }}" alt="" class="img-responsive">
+              </li>
             @endforeach
           </ul>
           <div class="col-sm-12 desc">
